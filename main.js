298d400399a1d44f123a009df40194a28eddf02c
MUHAMMAD ADNAN <160736300+adnansheikha@users.noreply.github.com>
2024-03-12 16:07:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var isbookavailable = true;
+var userborrowedbooks = 2;
+var maxAllowedbooks = 3;
+if (isbookavailable && userborrowedbooks < maxAllowedbooks) {
+    console.log("ap book borrow krsaktay hain!");
+}
+else {
+    console.log("ap book borrow n krsaktay ");
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+let isbookavailable : Boolean= true;
+let userborrowedbooks : number = 2;
+const maxAllowedbooks : number = 3;
+
+if ( isbookavailable && userborrowedbooks < maxAllowedbooks ) {
+    console.log ("ap book borrow krsaktay hain!");
+} else { 
+    console.log("ap book borrow n krsaktay " )
+}
